2aa963d2398d13566482681d82b5c23308aa1b39
최민서 <minseo00515@naver.com>
2021-09-05 12:42:45 +0900

    xml을 끌어와주는 LayoutInflater 변수 추가

--- app/src/main/java/com/nepplus/list_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/list_20210905/adapters/StudentAdapter.kt
@@ -1,6 +1,7 @@
 package com.nepplus.list_20210905.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import com.nepplus.list_20210905.datas.StudentData
 
@@ -8,4 +9,16 @@ class StudentAdapter(
     val mContext: Context,
     val resId: Int,
     val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext, resId, mList){
+
+
+//    한 줄에 해당하는 xml + StudentData클래스의 정보 결합 -> 리스트뷰의 각 자리에 뿌려주자
+//    xml로 그려둔 한 줄 모양을 => kt 안에서 활용할 수 있어야 한다.
+//    용을 도와주는 도구도 필요하다.
+
+        val mInflater = LayoutInflater.from(mContext)
+
+
+
+
+
 }
